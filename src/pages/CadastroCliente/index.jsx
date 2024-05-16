6687a68c6ce4796ee9beb9a0f411ede5eddd224a
Milena Rickli Silvério <141282223+MilenaRickliS@users.noreply.github.com>
2024-05-16 17:42:27 -0300

    c

--- src/pages/CadastroCliente/index.jsx
+++ src/pages/CadastroCliente/index.jsx
@@ -5,6 +5,11 @@ function CadastroCliente() {
   return (
       <div>
         <div><Header/></div>
+        <main id="main" class="flexbox-col">
+          <h2>Clientes</h2>
+          <p>Aqui está uma lista de todos os seus clientes!</p>
+          
+        </main> 
       </div>
   );
 }
--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -9,12 +9,10 @@ function Inicio() {
   
   return (
       <div>
-        <div><Header/>
-
-        
+        <div><Header/>        
         <main id="main" class="flexbox-col">
-          <h2>Lorem ipsum!</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum corporis, rerum doloremque iste sed voluptates omnis molestias molestiae animi recusandae labore sit amet delectus ad necessitatibus laudantium qui! Magni quisquam illum quaerat necessitatibus sint quibusdam perferendis! Aut ipsam cumque deleniti error perspiciatis iusto accusamus consequuntur assumenda. Obcaecati minima sed natus?</p>
+          <h2>Estoque</h2>
+          <p>Atualize sempre que possível o seu estoque!</p>
           
         </main>        
         </div>
