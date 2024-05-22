a468f4917e635a0b65f51b7dd943e6e53fd4c437
Milena Rickli Silvério <141282223+MilenaRickliS@users.noreply.github.com>
2024-05-22 01:10:24 -0300

    c

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,39 +1,8 @@
-import { Link } from 'react-router-dom';
+
 import './style.css';
-import AppContext from '../../contexts/AppContext'
-import { useState, useEffect, useContext, useMemo } from 'react';
-import { db } from '../../services/firebaseConnection';
-import { 
-  collection,
-  onSnapshot
-} from 'firebase/firestore';
 
-function Footer(){
-  //context para armazenar a quantidade de ração dos clientes
-  const { quantTotal, setQuantTotal, totalRef } = useContext(AppContext);
 
-  // Sum of all quantTotal values
-  const totalQuantTotal = useMemo(() => {
-    return quantTotal.reduce((acc, item) => acc + item.quantRacaoMes, 0);
-  }, [quantTotal]);
-
-  // Efeito que carrega os clientes do Firestore sempre que o componente é montado.
-  useEffect(() => {
-    async function loadQuant(){
-      const unsub = onSnapshot(collection(db, "clientes-di"), (snapshot) => {
-        let lista = [];
-        snapshot.forEach((doc) => {
-          lista.push({
-            quantRacaoMes: doc.data().quantRacaoMes, // use doc.data().quantRacaoMes instead of doc.quantRacaoMes
-          })
-        })
-        setQuantTotal(lista);
-      })
-    }
-    loadQuant();
-  }, [])
-
-  
+function Footer(){ 
 
   return(
     <footer>
@@ -44,8 +13,6 @@ function Footer(){
                         <ul>
                             <li>Bem-vindo ao site!</li>
                             <li>Controle de estoque de Ração para seus clientes!</li>
-                            <li>Total de ração: {totalQuantTotal} kg</li>
-                            <li></li>
                         </ul>
                     </nav>
                     {/* <nav className="menu-footer">
